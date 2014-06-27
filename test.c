e8cce36f5a1babb451ba583519fd384736458f2b
Adam Bławat <blawat.adam@gmail.com>
2014-06-27 17:36:16 +0200

    Dodałem funkcje testujące.

--- test.c
+++ test.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "linked-list-int.h"
+
+void list_create_test()
+{
+    linked_list_uint_t **list = linked_list_uint_create();
+    
+    assert(list[0] == NULL);
+    assert(list[1] == NULL);
+}
+
+void list_add_test()
+{
+    linked_list_uint_t **list;
+    
+    unsigned int value1 = 1;
+    unsigned int value2 = 2;
+    
+    
+    list = linked_list_uint_create();
+    
+    linked_list_uint_add_first(list, value1);
+    
+    assert(list[0] == list[1]);
+    assert(list[0] != NULL);
+    assert(list[0] -> value == value1);
+    assert(list[0] -> next  == NULL);
+    
+    linked_list_uint_add_first(list, value2);
+    
+    assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] != list[1]);
+    
+    assert(list[0] -> value == value2);
+    assert(list[0] -> next  == list[1]);
+    assert(list[1] -> value == value1);
+    assert(list[1] -> next  == NULL);
+    
+    
+    list = linked_list_uint_create();
+    
+    linked_list_uint_add_last(list, value1);
+    
+    assert(list[0] == list[1]);
+    assert(list[0] != NULL);
+    assert(list[0] -> value == value1);
+    assert(list[0] -> next  == NULL);
+    
+    linked_list_uint_add_last(list, value2);
+    
+    assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] != list[1]);
+    
+    assert(list[0] -> value == value1);
+    assert(list[0] -> next  == list[1]);
+    assert(list[1] -> value == value2);
+    assert(list[1] -> next  == NULL);
+}
 
 int main()
 {
-    unsigned int value1 = 1;
-    unsigned int value2 = 2;
-    unsigned int value3 = 3;
-    
-    linked_list_uint_t **list = linked_list_uint_create();
-    
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_first(list, value1);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_last(list, value2);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_last(list, value3);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_first(list, value3);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_last(list, value1);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_clear(list);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_last(list, value1);
-    linked_list_uint_print(list);
-    putchar(10);
-    
-    linked_list_uint_add_first(list, value3);
-    linked_list_uint_print(list);
-    putchar(10);
+    list_create_test();
+    list_add_test();
     
     return 0;
 }
