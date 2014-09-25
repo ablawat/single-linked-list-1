aff3e95df91a9fd48ec5997a4e2fb272d8e63ce8
Adam Bławat <blawat.adam@gmail.com>
2014-09-25 14:11:34 +0200

    Zmiana nazw niektórych zmiennych.

--- linked-list-int.c
+++ linked-list-int.c
@@ -17,23 +17,23 @@ linked_list_int_t ** linked_list_int_create()
 // --------------------------------
 int linked_list_int_add_first(linked_list_int_t **list, int value)
 {
-    linked_list_int_t *new_value = malloc(sizeof(linked_list_int_t));
+    linked_list_int_t *new_item = malloc(sizeof(linked_list_int_t));
     int result;
     
-    if (new_value != NULL)
+    if (new_item != NULL)
     {
-        new_value -> value = value;
+        new_item -> value = value;
         
         if (list[0] == NULL)
         {
-            new_value -> next = NULL;
-            list[0] = new_value;
-            list[1] = new_value;
+            new_item -> next = NULL;
+            list[0] = new_item;
+            list[1] = new_item;
         }
         else
         {
-            new_value -> next = list[0];
-            list[0] = new_value;
+            new_item -> next = list[0];
+            list[0] = new_item;
         }
         
         result = 0;
@@ -50,23 +50,23 @@ int linked_list_int_add_first(linked_list_int_t **list, int value)
 // --------------------------------
 int linked_list_int_add_last(linked_list_int_t **list, int value)
 {
-    linked_list_int_t *new_value = malloc(sizeof(linked_list_int_t));
+    linked_list_int_t *new_item = malloc(sizeof(linked_list_int_t));
     int result;
     
-    if (new_value != NULL)
+    if (new_item != NULL)
     {
-        new_value -> value = value;
-        new_value -> next  = NULL;
+        new_item -> value = value;
+        new_item -> next  = NULL;
         
         if (list[0] == NULL)
         {
-            list[0] = new_value;
-            list[1] = new_value;
+            list[0] = new_item;
+            list[1] = new_item;
         }
         else
         {
-            list[1] -> next = new_value;
-            list[1] = new_value;
+            list[1] -> next = new_item;
+            list[1] = new_item;
         }
         
         result = 0;
@@ -83,13 +83,13 @@ int linked_list_int_add_last(linked_list_int_t **list, int value)
 // ------------------------------
 void linked_list_int_clear(linked_list_int_t **list)
 {
-    linked_list_int_t *tmp_value = list[0];
+    linked_list_int_t *list_item = list[0];
     linked_list_int_t *to_remove;
     
-    while (tmp_value != NULL)
+    while (list_item != NULL)
     {
-        to_remove = tmp_value;
-        tmp_value = tmp_value -> next;
+        to_remove = list_item;
+        list_item = list_item -> next;
         free(to_remove);
     }
     
@@ -101,15 +101,16 @@ void linked_list_int_clear(linked_list_int_t **list)
 // -------------------------
 void linked_list_int_print(linked_list_int_t **list)
 {
-    linked_list_int_t *tmp_value = list[0];
+    linked_list_int_t *list_item = list[0];
     
     printf("L");
     
-    while (tmp_value != NULL)
+    while (list_item != NULL)
     {
         printf("-|");
-        printf("%d", tmp_value -> value);
+        printf("%d", list_item -> value);
         printf("|");
-        tmp_value = tmp_value -> next;
+        
+        list_item = list_item -> next;
     }
 }
